c0f26cc156584b92a8ebf4b486a763f93483b936
Michal Čihař <michal@cihar.com>
2024-11-25 10:32:09 +0100

    Introduce ruff code checker and formatter
    - make code formatting consistent
    - discover issues like #6 earlier

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='altcha',
-    version='0.1.5',
-    description='A library for creating and verifying challenges for ALTCHA.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Daniel Regeci',
-    author_email='536331+ovx@users.noreply.github.com',
-    url='https://github.com/altcha-org/altcha-lib-py',
+    name="altcha",
+    version="0.1.5",
+    description="A library for creating and verifying challenges for ALTCHA.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Daniel Regeci",
+    author_email="536331+ovx@users.noreply.github.com",
+    url="https://github.com/altcha-org/altcha-lib-py",
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
         # Add any dependencies here
     ],
