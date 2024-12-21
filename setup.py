bea449c56370810ff525370bc745f8c7a0429310
Daniel Regeci <536331+ovx@users.noreply.github.com>
2024-12-21 08:14:42 +0700

    v0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="altcha",
-    version="0.1.6",
+    version="0.1.7",
     description="A library for creating and verifying challenges for ALTCHA.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
